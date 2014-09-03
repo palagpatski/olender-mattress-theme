36526c03c0fc8e31a8b356422ab335f4ecc9b3a8
palagpatski <albaparticle2005@yahoo.com>
2014-09-03 08:38:41 +0800

    This is a new branch based on Omega 4, although all these files are from a new build.

--- olender/theme-settings.php
+++ olender/theme-settings.php
@@ -1,16 +1,17 @@
-<?php // $Id$
- 
+<?php
+
+/**
+ * @file
+ * Theme settings file for the Olender theme.
+ */
+
+require_once dirname(__FILE__) . '/template.php';
+
+/**
+ * Implements hook_form_FORM_alter().
+ */
 function olender_form_system_theme_settings_alter(&$form, $form_state) {
-  $form['olender_settings'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Optional Settings')
-   );
-  $form['olender_settings']['olender_site_slogan_line_2'] = array(
-    '#type'          => 'textfield',
-    '#title'         => t('Site Slogan Line 2'),
-    '#default_value'  => theme_get_setting('olender_site_slogan_line_2'), 
-    '#description'   => t("If the site slogan needs to break over more than 1 line, add the second line of text here."),
-    '#size'  => 70, 
-    '#maxlength'  => 128, 
-  );
+  // You can use this hook to append your own theme settings to the theme
+  // settings form for your subtheme. You should also take a look at the
+  // 'extensions' concept in the Omega base theme.
 }
